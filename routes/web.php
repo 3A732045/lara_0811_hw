9c2feb5cd3a82efba22804f0423cb9d5d33aefab
李沐恩 <jessezzxxcc@gmail.com>
2020-10-26 19:46:50 +0800

    新增名為about.index的route,並編輯AboutController的index方法

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/',[App\Http\Controllers\HomeController::class,'index'])->name('home.index');
+Route::get('about',[App\Http\Controllers\HomeController::class,'index'])->name('about.index');
